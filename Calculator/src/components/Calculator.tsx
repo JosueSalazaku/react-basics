90f9282bcf4a87c2910d9fefb1a4843367a01a0d
Josué Salazaku <josuesalazaku@gmail.com>
2025-03-05 22:53:10 +0100

    Initial commit Calculator

---
+++ Calculator/src/components/Calculator.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import CalcDisplay from "./CalcDisplay";
+
+function Calculator() {
+
+    const [currentInput, setcurrentInput] = useState<string>("0");
+    const [result, setResult] = useState<string>("")
+
+    const handleCurrentInput = (event: React.MouseEvent<HTMLButtonElement>) => {
+    }
+
+
+  return (
+    <div className="border border-white rounded-xl w-[600px] h-[900px] items-center justify-center">
+      <CalcDisplay currentInput={currentInput} result={result} />
+    </div>
+  );
+}
+
+export default Calculator;
